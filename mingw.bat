d09c273090316e9dd754100a2adc14772600a7c5
Kevin Goodwin <fwmechanic@yahoo.com>
2014-11-22 12:11:17 -0800

    remove mingw*.bat files as these depend on where user 'installed' these toolsets

--- mingw.bat
+++
@@ -1,2 +0,0 @@
-@if not defined MINGW_NUWEN echo need to set MINGW_NUWEN!&&goto :eof
-@%MINGW_NUWEN%\set_distro_paths.bat
--- mingw64.bat
+++
@@ -1,2 +0,0 @@
-@if not defined MINGW_NUWEN echo need to set MINGW_NUWEN!&&goto :eof
-@%MINGW_NUWEN%x64\MinGW\set_distro_paths.bat
