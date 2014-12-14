2e9b939bb55024710e107d4e6d1e20362615a491
Kevin Goodwin <fwmechanic@yahoo.com>
2014-12-14 15:31:18 -0800

    make install_build_tools.sh less annoying

--- install_build_tools.sh
+++ install_build_tools.sh
@@ -1,5 +1,12 @@
 # intended to be sourced: . install_build_tools.sh
 
+# modify the make rules to use these versions of gcc/g++
+export CC=gcc-4.8
+export CXX=g++-4.8
+
+which $CC && which $CXX && return 0
+
+
 # gcc 4.9.x is the dflt version on *ubuntu 14.10, and seems to be
 #           a major memory hog (I've observed this w/MinGW too)
 # gcc 4.8.x is sufficient for our needs, and is much less of a
@@ -14,7 +21,3 @@ libboost-filesystem-dev \
 libboost-system-dev     \
 libncurses5-dev         \
 libpcre3-dev            \
-
-# modify the make rules to use these versions of gcc/g++
-export CC=gcc-4.8
-export CXX=g++-4.8
