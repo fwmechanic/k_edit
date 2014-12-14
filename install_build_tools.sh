2180525dfa73b2f48f2182ce9eb97b67a4594d24
Kevin Goodwin <fwmechanic@yahoo.com>
2014-12-13 21:11:30 -0800

    slight cleanup

--- install_build_tools.sh
+++ install_build_tools.sh
@@ -1,10 +1,20 @@
 # intended to be sourced: . install_build_tools.sh
 
-# gcc 4.9.x is the dflt version on *ubuntu 14.10, and seems to be REAL memory hog
-# gcc 4.8.x is sufficient for our needs, and is much less of a memory hog (thus runs faster on <= 2GB VMs)
+# gcc 4.9.x is the dflt version on *ubuntu 14.10, and seems to be
+#           a major memory hog (I've observed this w/MinGW too)
+# gcc 4.8.x is sufficient for our needs, and is much less of a
+#           memory hog (thus runs faster on <= 2GB VMs)
 
-sudo apt-get install -y gcc-4.8 g++-4.8 libncurses5-dev libpcre3-dev libboost-dev libboost-system-dev libboost-filesystem-dev ctags
+sudo apt-get install -y \
+exuberant-ctags         \
+g++-4.8                 \
+gcc-4.8                 \
+libboost-dev            \
+libboost-filesystem-dev \
+libboost-system-dev     \
+libncurses5-dev         \
+libpcre3-dev            \
 
-# following modify the make rules to use these versions of gcc/g++
+# modify the make rules to use these versions of gcc/g++
 export CC=gcc-4.8
 export CXX=g++-4.8
