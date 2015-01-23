6cc0f331a21efa64ff1bfac5b03906bfa326b31d
Kevin Goodwin <fwmechanic@yahoo.com>
2015-01-22 21:20:21 -0800

    further license/copyright changing

--- install_build_tools.sh
+++ install_build_tools.sh
@@ -1,3 +1,22 @@
+#
+# Copyright 2015 by Kevin L. Goodwin [fwmechanic@gmail.com]; All rights reserved
+#
+# This file is part of K.
+#
+# K is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# K is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along
+# with K.  If not, see <http:#www.gnu.org/licenses/>.
+#
+
 # intended to be sourced: . install_build_tools.sh
 
 # modify the make rules to use these versions of gcc/g++
