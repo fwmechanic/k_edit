daad4214aabd1a105722ebfe008c77aaa43512d4
Kevin Goodwin <fwmechanic@gmail.com>
2015-03-03 07:40:42 -0800

    Linux: maintain state files per host even if homedir is on NFS (i.e. shared)

--- main.cpp
+++ main.cpp
@@ -904,6 +904,16 @@ STATIC_FXN void InitEnvRelatedSettings() { enum { DD=1 };  // c_str()
    s_stateFileDir += PATH_SEP_STR HOME_SUBDIR_NM;   0 && DD && DBG( "2: %s", s_stateFileDir.c_str() );
    if( !IsDir( s_stateFileDir.c_str() ) ) { mkdirOk( s_stateFileDir.c_str() ); }
    if( !IsDir( s_stateFileDir.c_str() ) ) { fprintf( stderr, "mkdir(%s) FAILED\n", s_stateFileDir.c_str() ); exit( 1 ); }
+  #if !defined(_WIN32)
+   { char hnbuf[257]; hnbuf[0] = '\0'; // in case homedir is an NFS mount: add a level of indirection to achieve per-host editor state
+   if( 0 == gethostname( BSOB( hnbuf ) ) && hnbuf[0] ) {
+      s_stateFileDir += PATH_SEP_STR;   0 && DD && DBG( "3: %s", s_stateFileDir.c_str() );
+      s_stateFileDir += hnbuf;          0 && DD && DBG( "3: %s", s_stateFileDir.c_str() );
+      if( !IsDir( s_stateFileDir.c_str() ) ) { mkdirOk( s_stateFileDir.c_str() ); }
+      if( !IsDir( s_stateFileDir.c_str() ) ) { fprintf( stderr, "mkdir(%s) FAILED\n", s_stateFileDir.c_str() ); exit( 1 ); }
+      }
+   }
+  #endif
    s_stateFileDir += PATH_SEP_CH;                        DD && DBG( "s_stateFileDir = '%s'", s_stateFileDir.c_str() );
    PutEnvOk( "K_STATEDIR", s_stateFileDir.c_str() );
    }
