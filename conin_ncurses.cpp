c1178c316d110c7c58d6b9cfbfc82617c2029e7a
Kevin Goodwin <fwmechanic@gmail.com>
2015-12-21 13:10:15 -0800

    ncurses: fix EdKC_tab assignment to graphic edfxn

--- conin_ncurses.cpp
+++ conin_ncurses.cpp
@@ -267,7 +267,7 @@ STATIC_FXN EdKC_Ascii GetEdKC_Ascii( bool fFreezeOtherThreads ) { // PRIMARY API
                }
             else {
                EdKC_Ascii rv;
-               rv.Ascii    = ev;
+               rv.Ascii    = ev == EdKC_tab ? '\t' : ev;  // hack alert!
                rv.EdKcEnum = ev;
                return rv;  // normal exit path
                }
