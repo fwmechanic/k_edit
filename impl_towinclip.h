105ced855b1c64eb64f7ee566ed7300413d11fe8
Kevin Goodwin <fwmechanic@gmail.com>
2016-02-04 08:39:11 -0800

    {}, vcompress

--- impl_towinclip.h
+++ impl_towinclip.h
@@ -21,7 +21,6 @@ bool ARG::towinclip() {
    hglbCopy_t      hglbCopy;
    PChar           bufptr; // this WILL TRAVERSE a buffer
    std::string     stbuf;
-
    if(  d_argType == NOARG
      || d_argType == LINEARG
      || d_argType == BOXARG
@@ -32,8 +31,9 @@ bool ARG::towinclip() {
       PCChar srcNm(nullptr);
       if( d_argType == NOARG ) {
          0 && DBG( "NOARG->%sClip", DestNm() );
-         if( g_pFbufClipboard->LineCount() == 0 )
+         if( g_pFbufClipboard->LineCount() == 0 ) {
             return ErrPause( "<clipboard> is empty!" );
+            }
          pFBuf = g_pFbufClipboard;
          yMin  = 0;
          yMax  = pFBuf->LastLine();
@@ -50,21 +50,19 @@ bool ARG::towinclip() {
          yMax   = d_boxarg.flMax.lin;
          }
       Msg( "%s->%sClip %d lines", srcNm ? srcNm : ArgTypeName(), DestNm(), (yMax - yMin)+1 );
-
       if( yMax == yMin ) {
          pFBuf->DupLineSeg( stbuf, yMin, xLeft, xRight ); // read line into our buffer
          goto SINGLE_LINE; // HACK O'RAMA!
          }
-
       { // determine # of chars on each line
       long size(0);
       for( auto lineNum(yMin); lineNum <= yMax; ++lineNum ) {
          pFBuf->DupLineSeg( stbuf, lineNum, xLeft, xRight );
          size += stbuf.length() + sizeof_eol();
          }
-
-      if( nullptr == (bufptr=PrepClip( 1+size, hglbCopy )) ) // ++ for trailing '\0'
+      if( nullptr == (bufptr=PrepClip( 1+size, hglbCopy )) ) { // ++ for trailing '\0'
          return false;
+         }
       }
       // copy source data into into *bufptr
       for( auto lineNum(yMin); lineNum <= yMax; ++lineNum ) {
@@ -86,15 +84,14 @@ bool ARG::towinclip() {
 SINGLE_LINE: // HACK O'RAMA!
       const auto blen( stbuf.length() );
       Msg( "%s(%" PR_SIZET "u)->%sClip:\"%s\"", ArgTypeName(), blen, DestNm(), stbuf.c_str() );
-      if( nullptr == (bufptr=PrepClip( 1+blen, hglbCopy )) )
+      if( nullptr == (bufptr=PrepClip( 1+blen, hglbCopy )) ) {
          return false;
-
+         }
       memcpy( bufptr, stbuf.c_str(), 1+blen );
       }
    else {
       return BadArg();
       }
-
    const auto errmsg( WrToClipEMsg( hglbCopy ) );
    if( errmsg ) {
       return ErrPause( "%s", errmsg );
