383cdac84a54fbbf639418e2d0059d4d58c066f4
Kevin Goodwin <fwmechanic@gmail.com>
2016-10-27 08:19:48 -0700

    correct indentation defects noticed by GCC 6.1.0

--- lua-5.1/src/ltablib.c
+++ lua-5.1/src/ltablib.c
@@ -137,7 +137,7 @@ static void addfield (lua_State *L, luaL_Buffer *b, int i) {
   if (!lua_isstring(L, -1))
     luaL_error(L, "invalid value (%s) at index %d in table for "
                   LUA_QL("concat"), luaL_typename(L, -1), i);
-    luaL_addvalue(b);
+  luaL_addvalue(b);
 }
 
 
@@ -284,4 +284,3 @@ LUALIB_API int luaopen_table (lua_State *L) {
   luaL_register(L, LUA_TABLIBNAME, tab_funcs);
   return 1;
 }
-
