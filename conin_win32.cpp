a9015e296eadba7ce42ef91fb6bad194aa442c80
Kevin Goodwin <fwmechanic@gmail.com>
2017-11-24 10:39:49 -0800

    copyright date update, a few comments
    git log --pretty=format: --name-only --since="326 days" | sort | uniq
    
    https://stackoverflow.com/a/7500276

--- conin_win32.cpp
+++ conin_win32.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright 2015-2016 by Kevin L. Goodwin [fwmechanic@gmail.com]; All rights reserved
+// Copyright 2015-2017 by Kevin L. Goodwin [fwmechanic@gmail.com]; All rights reserved
 //
 // This file is part of K.
 //
