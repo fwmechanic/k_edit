8ac55210dfb150a0b8d8637c1fb89266e96ff7c5
Kevin Goodwin <fwmechanic@gmail.com>
2023-02-11 08:40:00 -0700

    Add '*' to the charset which ARG::graphic wraps a BOXARG

--- fbuf_edit.cpp
+++ fbuf_edit.cpp
@@ -792,17 +792,17 @@ bool ARG::graphic() { enum { SD=0 };
    const char usrChar( d_pCmd->d_argData.chAscii() );
    // <000612> klg Finally did this!  Been needing it for YEARS!
    // g_delims, g_delimMirrors
-   //                                                m4 `quoting'
-   //                                                |
-   STATIC_CONST char chOpeningDelim[]    = "_%'\"(<{[`";
-   STATIC_CONST char chClosingDelim[]    = "_%'\")>}]`";
-   STATIC_CONST char chClosingDelim_m4[] = "_%'\")>}]'";
+   //                                                 m4 `quoting'
+   //                                                 |
+   STATIC_CONST char chOpeningDelim[]    = "*_%'\"(<{[`";
+   STATIC_CONST char chClosingDelim[]    = "*_%'\")>}]`";
+   STATIC_CONST char chClosingDelim_m4[] = "*_%'\")>}]'";
    const auto ixMatch( stref(chOpeningDelim).find( usrChar ) );
    const char chClosing( ixMatch==stref::npos ? '\0' : (g_fM4backtickquote ? chClosingDelim_m4 : chClosingDelim)[ ixMatch ] );
    std::string tmp1, tmp2;
    if( d_argType == BOXARG ) {
       if( usrChar == ' ' ) { // insert spaces
-         linsert();
+         PCFV_BoxInsertBlanks( d_boxarg );
          return true;
          }
       if( chClosing ) {
